db2fe16dc2bf8bef6d91b1d0c6ca331661affcfe
aashutosh148 <aashutoshsingh468847@gmail.com>
2024-07-11 23:27:38 +0530

    Added project list

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,31 +12,31 @@ const Projects = () => {
       githubLink: "https://github.com/aashutosh148/PlayList_Stat"
     },
     {
-      title: "Headlines",
-      description: "Integrated an external API to fetch and cache realtime news headlines on a backend server. Use React on the frontend to display headlines.",
-      image: "./Projects/headlines.png",
+      title: "Attendance & Vacation Panner",
+      description: "It helps students calculate fall in their attendance in case they take leave. Also Help them to get longest Holiday length with least Off Days",
+      image: "./Projects/vacation.png",
+      liveLink: "https://aashutosh148.github.io/UniHelp/resources/HolidayPlanner/",
+      githubLink: "https://github.com/aashutosh148/UniHelp"
+    },
+    {
+      title: "Chat Server & user-auth",
+      description: "Chat Application, with secure user-auth and real-time chat functionality. This project is built using Node.js, Express.js, MongoDB, Socket.IO, and HBS",
+      image: "./Projects/chatServ.png",
       liveLink: null,
-      githubLink: "https://github.com/aashutosh148/Headlines"
+      githubLink: "https://github.com/aashutosh148/ChatRoom-with-auth"
+    },
+    {
+      title: "User-Authentication With JWT",
+      description: "User authentication using JWT, endpoints for user registration, login/logout, access to protected routes, token revocation and token expiration.",
+      image: "./Projects/jwt.png",
+      liveLink: null,
+      githubLink: "https://github.com/aashutosh148/User-auth-JWT"
     },
     {
       title: "Headlines",
       description: "Integrated an external API to fetch and cache realtime news headlines on a backend server. Use React on the frontend to display headlines.",
       image: "./Projects/headlines.png",
       liveLink: null,
-      githubLink: "https://github.com/aashutosh148/Headlines"
-    },
-    {
-      title: "Headlines",
-      description: "Integrated an external API to fetch and cache realtime news headlines on a backend server. Use React on the frontend to display headlines.",
-      image: "./Projects/headlines.png",
-      liveLink: null,
-      githubLink: "https://github.com/aashutosh148/Headlines"
-    },
-    {
-      title: "Headlines",
-      description: "Integrated an external API to fetch and cache realtime news headlines on a backend server. Use React on the frontend to display headlines.",
-      image: "./Projects/headlines.png",
-      liveLink: 'njkb',
       githubLink: "https://github.com/aashutosh148/Headlines"
     }
   ];
